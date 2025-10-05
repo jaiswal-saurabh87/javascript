67cb198531a3a9b0ae60f0240fe5cd2cb87cc56a
Saurabh Jaiswal <saurabh.jaiswal.8438@gmail.com>
2025-10-05 13:51:19 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 console.log("Hello World!");
 let name = "Saurabh !";
 console.log(name);
+import java.io.*;
+import java.util.Scanner;
+public class main ()
+{
+  public static void main(String[]args)
+  {
+    System.out.println("Hello World in JAVA!");
+  }
+}
